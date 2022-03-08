26d8ef7435d920b9d4c563ba1a276f6629c8964c
Arun <arunjoseph001@gmail.com>
2022-03-08 17:56:43 +0530

    Updated the files

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const button = document.querySelector("button");
 let currentSquare = "none";
 let row = 15;
 
-function makeBoard(container) {
-  let gameWidth = row * 50 + 2;
+function makeBoard(container, rows) {
+  let gameWidth = rows * 50 + 2;
   gameWidth = gameWidth + "px";
   game.style.width = gameWidth;
-  let length = row * row;
+  let length = rows * rows;
   for (let i = 0; i < length; i++) {
     const row = md();
     // Array.from({ length }).forEach(() => row.appendChild(md()));
@@ -16,15 +16,15 @@ function makeBoard(container) {
   }
 }
 
-makeBoard(game);
+makeBoard(game,row);
 
 game.addEventListener("click", changeColor);
 
 function changeColor(event) {
   console.log(event.target);
   currentSquare = event.target;
-  let id=event.target.id;
-  if(id=='board'){
+  let id = event.target.id;
+  if (id == "board") {
     // console.log("clicked on the board now");
     return;
   }
@@ -34,7 +34,13 @@ function changeColor(event) {
 button.addEventListener("click", reset);
 
 function reset() {
-  console.log(button);
+  let count = prompt("Enter the numberof rows:-");
+  console.log(isNaN(+count));
   game.textContent = "";
-  makeBoard(game);
+  if (Number.isInteger(count)) {
+    if (count < 50) {
+      row = count;
+    }
+  }
+  makeBoard(game, row);
 }
